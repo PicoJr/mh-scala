cd7fa621f842852b237d5129db4aa8a69985ffb4
PicoJr <ncls_olg@yahoo.fr>
2017-11-08 21:38:44 +0100

    Make ItemFactory a companion for Item

--- src/main/scala-2.12/item/InventoryDefault.scala
+++ src/main/scala-2.12/item/InventoryDefault.scala
@@ -11,51 +11,51 @@ class InventoryDefault extends Inventory {
   override def getItems: Seq[Item] = items
 
   override def getCharmSlots: Int = {
-    getItemsEquipped.foldLeft(0)((sum, i) => sum + ItemFactory.getCharmSlots(i))
+    getItemsEquipped.foldLeft(0)((sum, i) => sum + Item.getCharmSlots(i))
   }
 
   override def getWeaponEquipped: Option[Item] = {
-    items.find(i => ItemFactory.isWeapon(i) & ItemFactory.isEquipped(i))
+    items.find(i => Item.isWeapon(i) & Item.isEquipped(i))
   }
 
   override def getItemsEquipped: Seq[Item] = {
-    items.filter(i => ItemFactory.isEquipped(i))
+    items.filter(i => Item.isEquipped(i))
   }
 
   override def getArmorProvided: Int = {
     val equipped : Seq[Item] = getItemsEquipped
-    equipped.foldLeft(0)((sum,i) => sum + ItemFactory.getArmor(i))
+    equipped.foldLeft(0)((sum, i) => sum + Item.getArmor(i))
   }
 
   override def getDamageProvided: Int = {
     val equipped : Seq[Item] = getItemsEquipped
-    equipped.foldLeft(0)((sum, i) => sum + ItemFactory.getRawDamage(i))
+    equipped.foldLeft(0)((sum, i) => sum + Item.getRawDamage(i))
   }
 
   override def getAttackElementType: ElementType = {
     if (getWeaponEquipped.nonEmpty) {
-      ItemFactory.getElementType(getWeaponEquipped.get)
+      Item.getElementType(getWeaponEquipped.get)
     } else {
       ElementType.NONE
     }
   }
 
   override def getArmorElementTypes: Seq[ElementType] = {
-    val armorPartsEquipped : Seq[Item] = items.filter(i => ItemFactory.isArmor(i) && ItemFactory.isEquipped(i))
-    armorPartsEquipped.map(i => ItemFactory.getElementType(i))
+    val armorPartsEquipped: Seq[Item] = items.filter(i => Item.isArmor(i) && Item.isEquipped(i))
+    armorPartsEquipped.map(i => Item.getElementType(i))
   }
 
   override def getAttackStatusType: StatusType = {
     if (getWeaponEquipped.nonEmpty) {
-      ItemFactory.getStatusType(getWeaponEquipped.get)
+      Item.getStatusType(getWeaponEquipped.get)
     } else {
       StatusType.NONE
     }
   }
 
   override def getArmorStatusTypes: Seq[StatusType] = {
-    val armorPartsEquipped: Seq[Item] = items.filter(i => ItemFactory.isArmor(i) && ItemFactory.isEquipped(i))
-    armorPartsEquipped.map(i => ItemFactory.getStatusType(i))
+    val armorPartsEquipped: Seq[Item] = items.filter(i => Item.isArmor(i) && Item.isEquipped(i))
+    armorPartsEquipped.map(i => Item.getStatusType(i))
   }
 
   override def addItems(items: Item*): Unit = {
--- src/main/scala-2.12/item/Item.scala
+++ src/main/scala-2.12/item/Item.scala
@@ -8,7 +8,7 @@ import item.StatusType.StatusType
   * Created by nol on 04/11/17.
   */
 sealed class Item(name: String) {
-  private final val uniqueID = ItemFactory.getNewUniqueItemID
+  private final val uniqueID = Item.getNewUniqueItemID
 
   def getName: String = name
 
@@ -21,9 +21,63 @@ sealed class Item(name: String) {
   }
 }
 
+object Item {
+
+  private var itemID: Long = 0
+
+  def getNewUniqueItemID: Long = {
+    itemID += 1
+    itemID
+  }
+
+  def isWeapon(i: Item): Boolean = {
+    i.isClassifiedAs(Classification.DAMAGE, Classification.EQUIPMENT)
+  }
+
+  def isArmor(i: Item): Boolean = {
+    i.isClassifiedAs(Classification.PROTECTION, Classification.EQUIPMENT)
+  }
+
+  def getRawDamage(i: Item): Int = {
+    if (i.isClassifiedAs(Classification.DAMAGE)) {
+      i.asInstanceOf[Damage].getRawDamage
+    } else 0
+  }
+
+  def getArmor(i: Item): Int = {
+    if (i.isClassifiedAs(Classification.PROTECTION)) {
+      i.asInstanceOf[Protection].getArmor
+    } else 0
+  }
+
+  def isEquipped(i: Item): Boolean = {
+    if (i.isClassifiedAs(Classification.EQUIPMENT)) {
+      i.asInstanceOf[Equipment].isEquipped
+    } else false
+  }
+
+  def getElementType(i: Item): ElementType = {
+    if (i.isClassifiedAs(Classification.ELEMENT)) {
+      i.asInstanceOf[Element].getElementType
+    } else ElementType.NONE
+  }
+
+  def getStatusType(i: Item): StatusType = {
+    if (i.isClassifiedAs(Classification.STATUS)) {
+      i.asInstanceOf[Status].getStatusType
+    } else StatusType.NONE
+  }
+
+  def getCharmSlots(i: Item): Int = {
+    if (i.isClassifiedAs(Classification.CHARM_SLOT)) {
+      i.asInstanceOf[CharmSlot].getCharmSlots
+    } else 0
+  }
+}
+
 class ItemDecorator(i: Item, c: Classification*) extends Item(i.getName) {
   val item: Item = i
-  private final val uniqueDecoratorID = ItemFactory.getNewUniqueItemID
+  private final val uniqueDecoratorID = Item.getNewUniqueItemID
 
   override def getUniqueID: Long = uniqueDecoratorID
 
