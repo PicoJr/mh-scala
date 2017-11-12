884bde6f49832cb89e1a8d7a7a1fc66c088b629b
PicoJr <ncls_olg@yahoo.fr>
2017-11-12 12:12:10 +0100

    Make parser more fault tolerant

--- src/main/scala-2.12/console/CommandParser.scala
+++ src/main/scala-2.12/console/CommandParser.scala
@@ -1,6 +1,7 @@
 package console
 
 import game.GameState
+import org.rogach.scallop.exceptions._
 import org.rogach.scallop.{ScallopConf, ScallopOption, Subcommand}
 
 /**
@@ -9,38 +10,49 @@ import org.rogach.scallop.{ScallopConf, ScallopOption, Subcommand}
 class CommandParser(command: Command) {
 
   def runCommand(args: Seq[String], gameState: GameState): Unit = {
-    val conf = new Conf(args)
-    conf.subcommands match {
-      case List(conf.hunter) => command.showHunter(gameState)
-      case List(conf.inventory) => command.listInventory(gameState)
-      case List(conf.quests, conf.quests.list) => command.listQuests(gameState)
-      case List(conf.quests, conf.quests.show) => command.showQuest(gameState, conf.quests.show.id.toOption.get)
-      case List(conf.quests, conf.quests.start) => command.startQuest(gameState, conf.quests.start.id.toOption.get)
-      case _ => println(conf.subcommands)
+    try {
+      val conf = new Conf(args)
+      conf.subcommands match {
+        case List(conf.hunter) => command.showHunter(gameState)
+        case List(conf.inventory) => command.listInventory(gameState)
+        case List(conf.quests, conf.quests.list) => command.listQuests(gameState)
+        case List(conf.quests, conf.quests.show) => command.showQuest(gameState, conf.quests.show.id.toOption.get)
+        case List(conf.quests, conf.quests.start) => command.startQuest(gameState, conf.quests.start.id.toOption.get)
+        case List(conf.quit) => println("quit")
+        case _ =>
+      }
+    } catch {
+      case ScallopException(message) => System.err.println(s"$message")
     }
   }
 
   class Conf(arguments: Seq[String]) extends ScallopConf(arguments) {
     val hunter = new Subcommand("hunter")
+    addSubcommand(hunter)
     val inventory = new Subcommand("inventory")
+    addSubcommand(inventory)
     val quests = new Subcommand("quests") {
       val list = new Subcommand("ls")
+      addSubcommand(list)
       val show = new Subcommand("show") {
         val id: ScallopOption[Int] = trailArg[Int]("questID")
       }
+      addSubcommand(show)
       val start = new Subcommand("start") {
         val id: ScallopOption[Int] = trailArg[Int]("questID")
       }
-      addSubcommand(list)
-      addSubcommand(show)
       addSubcommand(start)
     }
-    val quit = new Subcommand("quit")
-    addSubcommand(hunter)
-    addSubcommand(inventory)
     addSubcommand(quests)
+    val quit = new Subcommand("quit")
     addSubcommand(quit)
     verify()
+
+    override def onError(e: Throwable): Unit = e match {
+      case Help("") => builder.printHelp
+      case Help(subName) => builder.findSubbuilder(subName).get.printHelp()
+      case other => throw other
+    }
   }
 
 }
