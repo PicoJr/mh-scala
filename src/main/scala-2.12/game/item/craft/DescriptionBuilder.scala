d033e1b7ebf3a5b39964557870ec281becf14975
PicoJr <ncls_olg@yahoo.fr>
2017-12-21 16:20:04 +0100

    Remove outdated method

--- src/main/scala-2.12/game/item/craft/DescriptionBuilder.scala
+++ src/main/scala-2.12/game/item/craft/DescriptionBuilder.scala
@@ -4,26 +4,19 @@ package game.item.craft
   * Created by nol on 20/11/17.
   */
 class DescriptionBuilder {
-  var nature: String = "undefined"
-  var element: Option[String] = Option.empty
-  var adjectives: Seq[String] = Seq.empty
+  private var nature: String = "undefined"
+  private var adjectives: Seq[String] = Seq.empty
 
   def getDescription: String = {
     val description = new StringBuilder()
     for (adjective <- adjectives) {
       description.append(adjective).append(" ")
     }
-    element match {
-      case Some(e) => description.append(e).append(" ")
-      case None =>
-    }
     description.append(nature)
     description.toString()
   }
 
   def getNature: String = nature
-
-  def getElement: Option[String] = element
 
   def getAdjectives: Seq[String] = adjectives
 
@@ -34,11 +27,6 @@ class DescriptionBuilder {
 
   def addAdjective(adjective: String): DescriptionBuilder = {
     this.adjectives = adjectives :+ adjective
-    this
-  }
-
-  def addElement(element: String): DescriptionBuilder = {
-    this.element = Option(element)
     this
   }
 }
