26cb6f37474d19dc046162d6e770a24dade0d340
PicoJr <ncls_olg@yahoo.fr>
2017-12-28 21:51:15 +0100

    Add generic decorator (WIP)

---
+++ src/main/scala-2.12/game/item/DefaultDecorator.scala
@@ -0,0 +1,24 @@
+package game.item
+
+import game.item.element.ElementType
+import game.item.status.StatusType
+
+/**
+  * Created by nol on 28/12/17.
+  */
+class DefaultDecorator extends AbstractDecorator[ItemType] {
+
+  override def decorateWithCharmSlot(wrapped: ItemType, charmSlotsProvided: Int): ItemType = CharmSlot(wrapped, charmSlotsProvided)
+
+  override def decorateWithDamage(wrapped: ItemType, damage: Int): ItemType = Damage(wrapped, damage)
+
+  override def decorateWithElement(wrapped: ItemType, elementType: ElementType): ItemType = Element(wrapped, elementType)
+
+  override def decorateWithEquipment(wrapped: ItemType, slotTypeRequirements: SlotTypeRequirements): ItemType = Equipment(wrapped, slotTypeRequirements)
+
+  override def decorateWithMaterial(wrapped: ItemType): ItemType = Material(wrapped)
+
+  override def decorateWithProtection(wrapped: ItemType, armor: Int): ItemType = Protection(wrapped, armor)
+
+  override def decorateWithStatus(wrapped: ItemType, statusType: StatusType): ItemType = Status(wrapped, statusType)
+}
