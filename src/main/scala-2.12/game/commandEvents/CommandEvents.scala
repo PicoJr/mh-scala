a55745ba8b57a3d1aef6b2b7c1ef70d85f9d2605
PicoJr <ncls_olg@yahoo.fr>
2018-02-25 15:58:50 +0100

    Add comments for CommandEvents, DefaultGameStatistics

--- src/main/scala-2.12/game/commandEvents/CommandEvents.scala
+++ src/main/scala-2.12/game/commandEvents/CommandEvents.scala
@@ -3,35 +3,71 @@ package game.commandEvents
 import game.id.Identifiable
 import rescala._
 
-/**
+/** Commands (may not succeed: does not mean the action will be done).
   * Created by nol on 19/12/17.
   */
 class CommandEvents {
 
   type Id = Identifiable.Id
 
+  /**
+    * List quests.
+    */
   val listQuests: rescala.Evt[Unit] = Evt[Unit]()
 
+  /**
+    * List items from inventory.
+    */
   val listInventory: rescala.Evt[Unit] = Evt[Unit]()
 
+  /**
+    * Show craft recipes with item with id Id.
+    */
   val showCraft: rescala.Evt[Id] = Evt[Id]()
 
+  /**
+    * Craft using items with ids Id, Id.
+    */
   val craftItem: rescala.Evt[(Id, Id)] = Evt[(Id, Id)]()
 
+  /**
+    * Show quest with id Id.
+    */
   val showQuest: rescala.Evt[Id] = Evt[Id]()
 
+  /**
+    * Show hunter stats.
+    */
   val showHunter: rescala.Evt[Unit] = Evt[Unit]()
 
+  /**
+    * Show global stats.
+    */
   val showStat: rescala.Evt[Unit] = Evt[Unit]()
 
+  /**
+    * Equip item with id Id.
+    */
   val equipItem: rescala.Evt[Id] = Evt[Id]()
 
+  /**
+    * Un-equip item with id Id.
+    */
   val unEquipItem: rescala.Evt[Id] = Evt[Id]()
 
+  /**
+    * Show item with id Id.
+    */
   val showItem: rescala.Evt[Id] = Evt[Id]()
 
+  /**
+    * Rename hunter with name String.
+    */
   val renameHunter: rescala.Evt[String] = Evt[String]()
 
+  /**
+    * Start quest with id Id.
+    */
   val startQuest: rescala.Evt[Id] = Evt[Id]()
 
 }
--- src/main/scala-2.12/game/gameStatistics/DefaultGameStatistics.scala
+++ src/main/scala-2.12/game/gameStatistics/DefaultGameStatistics.scala
@@ -10,13 +10,28 @@ object DefaultGameStatistics {
 
   type Id = Identifiable.Id
 
+  /**
+    * Set of quests id successfully completed.
+    */
   val questSucceeded: Var[Set[Id]] = Var(Set.empty[Id])
 
+  /**
+    * Successfully completed quests count.
+    */
   val questSucceededCount: Var[Int] = Var(0)
 
+  /**
+    * Failed quests count.
+    */
   val questFailedCount: Var[Int] = Var(0)
 
+  /**
+    * Attempts count.
+    */
   val questStartedCount: Var[Int] = Var(0)
 
+  /**
+    * Item crafted count.
+    */
   val itemCraftedCount: Var[Int] = Var(0)
 }
